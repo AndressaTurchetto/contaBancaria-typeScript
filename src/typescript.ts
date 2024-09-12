00a0a9c3c220755c26052e5767f4f9fe79d345ad
Consórcio ASSES - COMPUTADORES <computadores@consorcioasses.onmicrosoft.com>
2024-09-12 14:59:58 -0300

    Criando funções com TypeScript

---
+++ src/typescript.ts
@@ -0,0 +1,13 @@
+// Tipos primitivos
+let valor: number = 3000;
+let nome: string = "";
+let isPago: boolean = false;
+let qualquer: any = "";
+qualquer = 22;
+
+// Tipos primitivos
+const lista: number[] = [];
+lista.push(13, 22.5, 22, 9);
+
+
+
